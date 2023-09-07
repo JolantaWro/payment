150e9d970f39e5925f5e514620d7ce19d57bdce7
JolantaWro <jolanta.woronowska@gmail.com>
2023-09-07 22:18:26 +0200

    tss

--- app/api/page.tsx
+++ app/api/page.tsx
@@ -13,18 +13,18 @@ import { NextResponse } from 'next/server';
 
 import { db } from '@vercel/postgres';
 
-export default async function handler(request:  Request, response: Response) {
-    const client = await db.connect();
+// export default async function handler(request:  Request, response: Response) {
+//     const client = await db.connect();
 
-    try {
-        await client.sql`CREATE TABLE IF NOT EXISTS Pets ( Name varchar(255), Owner varchar(255) );`
-        const names = ["Kama", "Peny"];
-        await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`
-    } catch (err) {
-        return NextResponse.json({ err }, { status: 500 });
-    }
+//     try {
+//         await client.sql`CREATE TABLE IF NOT EXISTS Pets ( Name varchar(255), Owner varchar(255) );`
+//         const names = ["Kama", "Peny"];
+//         await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`
+//     } catch (err) {
+//         return NextResponse.json({ err }, { status: 500 });
+//     }
 
-    const pets = await client.sql`SELECT * FROM Pets;`
-    NextResponse.json({ pets }, { status: 200 });
-}
+//     const pets = await client.sql`SELECT * FROM Pets;`
+//     NextResponse.json({ pets }, { status: 200 });
+// }
  
