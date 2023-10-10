ad4e96c0b826c5bbae7d39d7cae9ff706e5094a5
JolantaWro <jolanta.woronowska@gmail.com>
2023-10-10 22:07:30 +0200

    add bg as test

--- app/(application)/(auth)/layout.tsx
+++ app/(application)/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
     children: React.ReactNode;
   }) {
     return (
-      <main className="flex h-screen w-screen items-center justify-center">
+      <main className="flex h-screen w-screen items-center justify-center bg-gray-50">
         {children}
       </main>
     );
